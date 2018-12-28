ab9fbe2bac369ba69a16b190f160cfde38a76320
Juho Naalisvaara <jnaalisv@hal.local>
2018-12-28 11:09:14 +0200

    add http endpoint for creating a new customer

---
+++ src/test/java/com/acme/integration/http/CustomerControllerIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.acme.integration.http;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CustomerControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void aNewCustomerIsCreated() throws Exception {
+        mvc.perform(post("/customers")
+                .content("{\"name\":\"ComputerArtists Inc\"}"))
+                .andExpect(status().isOk());
+    }
+
+}
