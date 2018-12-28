383ff5dd46a81f6c0a39e8087b6a36c660bb334c
Juho Naalisvaara <jnaalisv@hal.local>
2018-12-28 10:55:35 +0200

    add Spring Boot web application

---
+++ src/main/java/com/acme/AcmeAccountingApp.java
@@ -0,0 +1,11 @@
+package com.acme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeAccountingApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AcmeAccountingApp.class, args);
+    }
+}
